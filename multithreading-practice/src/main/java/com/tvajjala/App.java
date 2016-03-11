dff2a0f4a9437b7c66f73919c125b786b11a0604
ThirupathiReddy V <tvajjala@innominds.com>
2016-03-11 10:11:35 +0530

    multitheading

---
+++ multithreading-practice/src/main/java/com/tvajjala/App.java
@@ -0,0 +1,13 @@
+package com.tvajjala;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
